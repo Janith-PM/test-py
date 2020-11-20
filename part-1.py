83798f02feac06fba3c68e2af084bfab7b6e66c1
Janith <janithpm9991@gmail.com>
2020-11-20 14:19:39 +0530

    part -1

---
+++ part-1.py
@@ -0,0 +1,24 @@
+# Part - 1 function for predict progression outcome
+def predict(pass_=0, defer=0, fail=0):
+
+    if(pass_ + defer + fail == 120):
+        if (pass_ == 120 and defer == 0 and fail == 0):
+            print('Progress')
+        elif(pass_ == 100 and 0 <= defer <= 20 and 0 <= fail <= 20):
+            print('Progress (module trailer)')
+        elif(0 <= pass_ <= 40 and 0 <= defer <= 40 and 80 <= fail <= 120):
+            print('Exclude')
+        elif(0 <= pass_ <= 80 and 0 <= defer <= 120 and 0 <= fail <= 60):
+            print('Do not progress - module retriever')
+        else:
+            print('error : invalied Entry')
+    else:
+        print('Total incorrect')
+
+
+# function calling
+pass_ = int(input('Please enter your credits at Pass : '))
+defer = int(input('Please enter your credits at defer : '))
+fail = int(input('Please enter your credits at fail : '))
+
+predict(pass_, defer, fail)
