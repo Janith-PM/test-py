fb9962907205c72ed9e078ef78094722c0fa88f4
Janith <janithpm9991@gmail.com>
2020-11-20 17:33:20 +0530

    final commit

--- withClass/main.py
+++ withClass/main.py
@@ -5,20 +5,19 @@ pr = predict.Predict()
 print('-' * 100)
 print('Staff Version With Historgram\n')
 
-if __name__ == '__main__':
+while True:
+    pass_, defer, fail = pr.getInput()
+    pr.prediction(pass_, defer, fail)
     while True:
-        pass_, defer, fail = pr.getInput()
-        pr.prediction(pass_, defer, fail)
-        while True:
-            cmd = input(
-                "\nWould you like to enter another set of data? \nEnter 'y' for Yes or 'q' to Quit and view result : ")
-            if cmd.lower() == 'q':
-                pr.horizontalHistrogram()
-                pr.verticalHistrogarm()
-                exit()
-            elif cmd.lower() == 'y':
-                print('')
-                break
-            else:
-                print('[x] Error : Invalied input')
-                continue
+        cmd = input(
+            "\nWould you like to enter another set of data? \nEnter 'y' for Yes or 'q' to Quit and view result : ")
+        if cmd.lower() == 'q':
+            pr.horizontalHistrogram()
+            pr.verticalHistrogarm()
+            exit()
+        elif cmd.lower() == 'y':
+            print('')
+            break
+        else:
+            print('[x] Error : Invalied input')
+            continue
